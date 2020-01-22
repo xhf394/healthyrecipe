2ad1e31ccf7ea438b80db441f6b3643ff06fed9b
xhf394 <39177879+xhf394@users.noreply.github.com>
2020-01-22 10:37:56 +0800

    Updates

---
+++ precache-manifest.29a5078beba0a70fe4be431b0b9ceea8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18135a35fbcc4fe143bab21fd3492573",
+    "url": "/healthyrecipe/index.html"
+  },
+  {
+    "revision": "0beadcd67ceef9ece9b4",
+    "url": "/healthyrecipe/static/css/main.acb273c4.chunk.css"
+  },
+  {
+    "revision": "f76b716cb0d59021454a",
+    "url": "/healthyrecipe/static/js/2.9a59cb31.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/healthyrecipe/static/js/2.9a59cb31.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0beadcd67ceef9ece9b4",
+    "url": "/healthyrecipe/static/js/main.a34a8d6d.chunk.js"
+  },
+  {
+    "revision": "a54798e54a558df04195",
+    "url": "/healthyrecipe/static/js/runtime-main.e38cfcab.js"
+  }
+]);
